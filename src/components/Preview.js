6fefee5e7aa7a4434ef3fd0f371a1dc48df09577
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-06-24 12:11:08 +0530

    heading color changed

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -11,7 +11,7 @@ class PreviewComponent extends React.Component {
       <div id="preview-main">
         <div id="preview-header">
           <div>
-            <Text fontSize={"3xl"} as={"b"} color={"black.600"}>
+            <Text fontSize={"3xl"} as={"b"} color={"teal.600"}>
               {generalComponent.name.toUpperCase()}
             </Text>
             <Text fontSize={"xl"}>{generalComponent.role}</Text>
