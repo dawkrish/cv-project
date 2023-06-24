b028ac4b8285aabd1db20ea393c60ebb15ebf0a3
krishnanshagarwal112 <gonkillua2680@gmail.com>
2023-06-24 11:32:11 +0530

    ..

--- src/components/Educations.js
+++ src/components/Educations.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Input } from "@chakra-ui/react";
 import { Flex } from "@chakra-ui/react";
-import { Textarea } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 
 class EducationComponent extends React.Component {
